5b931d72cbff5aa30e4d9e464937093863c98d29
Dam-pixel <73979404+Dam-pixel@users.noreply.github.com>
2020-12-19 19:11:32 +1300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Technologies/>
     </div>
+  );
+}
+
+const Header = () => {
+  return (<div>
+    <a href='#s'>Home</a>
+    <a href='#s'>Messages</a>
+    <a href='#s'>News Feed</a>
+  </div>
+  );
+}
+
+
+const Technologies = () => {
+  return (
+    <div>
+    <ul>
+      <li>
+        CSS
+    </li>
+      <li>
+        HTML
+    </li>
+      <li>
+        REACT
+    </li>
+    </ul>
+  </div>
+
   );
 }
 
